af4288342b34574741ade22bc2c7b3d186defe87
Antelope10 <86484092+Antelope10@users.noreply.github.com>
2023-01-11 09:07:36 -0600

    update

--- encrypt.py
+++ encrypt.py
@@ -33,29 +33,6 @@ def decrypt(md,priv_key):
     print(m)
     return m
 
-def generate_key_pair():
-    p = int(random.randrange(1000000000000000,2000000000000000)) #generate p,q,n
-    while not h.isPrime(p):
-        p = p +1
-    q = p+1
-    while not h.isPrime(q):
-        q += 1
-    n = p*q
-    
-    totient_n = (q -1) * (p-1) #calculate totient(n)
-    
-    e = random.randrange(50,100) #generate e coprime to n
-    while not h.coprime(totient_n,e):
-        e += 1
-        
-    d = h.modinv(e,totient_n) #calculate modular inverse of e with respect to totient(n)
-    
-    public_key = str(n) + "_" + str(e)
-    private_key = str(n)+ "_" + str(d)
-    print("Public Key: " + public_key)
-    print("Private Key: " + private_key)
-    return public_key, private_key
-
 def verifyKeys(pub_key, priv_key):
     testMessage = "hi12"
     out = encrypt(testMessage,pub_key)
@@ -66,43 +43,13 @@ pub_key = 0
 priv_key = 0
 
 def main():
-    while (1): #login/registration
-        print("Type in the number of the action you want to take:")
-        print("0 - Exit")
-        print("1 - Login")
-        print("2 - Register")
-        action = input("Enter: ")
-        if action == "0":
+    contents = ""
+    with open('in.txt', 'r') as infile:
+        for line in infile:
             return
-        elif action == "1":
-            pub_key = input("Public Key: ")
-            priv_key = input("Private Key: ")
-            if not verifyKeys(pub_key,priv_key):
-                print("Invalid Login")
-                pub_key = 0
-                priv_key = 0
-            else:
-                exit = 1
-        elif action == "2":
-            pub_key, priv_key = generate_key_pair()
-            exit = 1
-        if exit:
-            break  
-    exit = 0
-    
-    while (1): #account actions   
-        print("Type in the number of the action you want to take:")
-        print("0 - Exit")
-        print("1 - Encrypt")
-        print("2 - Decrypt")
-        print("3 - Sign")
-        action = int(input("Enter: "))
-        if action == 0:
-            return
-        elif action == 1:
-            encrypt(input("message: "),priv_key)
-        elif action == 2:
-            decrypt(input("cipher: "),pub_key)
+        with open('file.txt', 'w') as outfile:
+            outfile.write('Hello World!')
 
+    
 if __name__ == "__main__":
     main()
